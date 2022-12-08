1b28bb7642483497e09f267e3c2095446ec8a2d6
ElKi <elikingofficial@gmail.com>
2022-12-08 20:04:50 +0800

    Search bar work with enter key

--- files/js/search3.js
+++ files/js/search3.js
@@ -90,9 +90,24 @@ function Select() {
     wordItem.addEventListener("click", function (e) {
       inputElem.value = e.target.textContent;
       searchInput.classList.remove("active");
+      document.getElementById("search").focus();
     });
   });
 }
+
+// Get the input field
+var input = document.getElementById("search");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("isearch").click();
+  }
+});
 
 function openPage() {
   var search = document.getElementById("search").value;
